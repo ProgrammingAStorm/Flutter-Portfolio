ccb85d39b6a1f0800801fcd0fa6a6e951b5d2d17
Mark Pavel <markpavel02@gmail.com>
2023-03-01 16:25:20 -0500

    Updated Welcome

--- lib/pages/main_menu/components/welcome.dart
+++ lib/pages/main_menu/components/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
@@ -17,15 +18,31 @@ class Welcome extends StatelessWidget {
             margin: const EdgeInsets.all(20),
             child: Text(
               '''
-     Thank you for visiting my portfolio! I hope you enjoy the background effect that I have created. It was very fun and challenging to create.
-     If you would like to learn more about the effect, or if you would like to play around with some of its settings, then you can go to the Settings tab for more options. Otherwise, click on the other tabs for more information on those topics and to be navigated to the appropriate page.
-     Please forgive the lack of content as doing the whole from scratch while also learning flutter and explaining it took a lot out of me, and I have more immediately important things to work on. Such things inclue, my resume, fixing up old projects to put on my resume, a C# full-stack MVC project, in that order.
-     That being said, I have much planned for the continuation of this site, and I hope you will visit again as it continues to develop.
-     Thank you for your understanding, and please enjoy the site.''',
+     Thank you for visiting my portfolio! I hope you enjoy the background effects that I created. Please do enjoy tinkering with them, and look forward to more as, even though I'm not currently working on this page, I have much more planned to come.
+     
+     The mouse effect was a small idea that turned out to be big, but the sunrise/sunset effect stole my heart. I managed to finish it in a fraction of the time compared to the mouse effect, since I learned a lot the first time around, but I still had to start from scratch half-way through.
+     
+     Currently, I'm working on my resume, but I plan to add a lot to this site. No spoilers of course. ;)''',
               style: Theme.of(context).textTheme.bodySmall,
               textAlign: TextAlign.start,
             ),
           ),
+          Column(children: [
+            const Text("Check out this page's source code below:"),
+            TextButton(
+                onPressed: () async {
+                  final Uri url = Uri.parse(
+                      'https://github.com/ProgrammingAStorm/Flutter-Portfolio');
+
+                  if (!await launchUrl(url)) {
+                    throw Exception('Could not launch $url');
+                  }
+                },
+                child: Text(
+                  'https://github.com/ProgrammingAStorm/Flutter-Portfolio',
+                  style: Theme.of(context).textTheme.displaySmall,
+                ))
+          ]),
           Container(
             margin: const EdgeInsets.all(20),
             child: Text(
